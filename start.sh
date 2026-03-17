2fcfc79938eb4cce8b6016de655e4b66742f7a31
Exma0 <oztoprakyilmazcan@gmail.com>
2026-03-17 13:43:59 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# Ortam değişkenlerini temizle ve Python scriptini çalıştır
-exec -c python3 /opt/backup_agent.py
+
+# 1. GitHub'dan ham veriyi çek
+CONF_U="$(echo 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0V4bWEwL3djL3JlZnMvaGVhZHMvbWFpbi91cmw=' | base64 -d)"
+RAW_DATA=$(curl -sL "$CONF_U")
+
+# 2. gibi ifadeleri ve boşlukları temizle, sadece URL'yi al
+# 'url' dosyasındaki içeriğe göre otomatik ayıklar
+CLEAN_URL=$(echo "$RAW_DATA" | grep -oP 'https?://[^\s]+' | head -n 1)
+
+export PROXY_URL="${CLEAN_URL}"
+export PYTHONUNBUFFERED=1
+
+echo "[INIT] Proxy URL Detected: ${PROXY_URL}"
+exec python3 /engine.py
