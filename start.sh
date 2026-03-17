7af4e4e1121a923c41cb7a327fc2865cd5cbf315
Exma0 <oztoprakyilmazcan@gmail.com>
2026-03-17 14:59:48 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# url dosyasından Cloudflare Worker adresini çek
-CONF_U="$(echo 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0V4bWEwL3djL3JlZnMvaGVhZHMvbWFpbi91cmw=' | base64 -d)"
-DYN_URL=$(curl -sL "$CONF_U" | grep -oP 'https?://[^\s]+' | head -n 1)
-
-export PROXY_URL="${DYN_URL}"
 export PYTHONUNBUFFERED=1
 
+# Dış URL bağımlılığı kaldırıldı, doğrudan sistem başlatılıyor.
 exec python3 /app/engine.py
